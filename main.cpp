220d8545798460691ced2f6e1e2e66018f31cb6b
J-Wll <80954812+J-Wll@users.noreply.github.com>
2023-10-04 09:24:33 +0000

    Year input, quit option and formatting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <limits>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -8,24 +10,38 @@ using namespace std;
 // https://stackoverflow.com/questions/59656675/how-can-i-pass-a-condition-to-a-function-in-c
 
 bool makeValidate(int inp){return inp >= 1 && inp <= 4 ? true : false;}
+bool yearValidate(int inp){return inp >= 1995 && inp <= 2015 ? true : false;}
+// bool makeValidate(int inp){return inp >= 1 && inp <= 4 ? true : false;}
+
+string getMake(int inp){
+  switch(inp){
+    case 1: return "Ford";
+    case 2: return "Nisan";
+    case 3: return "Volvo";
+    case 4: return "Jaguar";
+  }
+}
 
 int getInp(string question, bool (*func)(int)) {
   int inp;
   bool fail = false;
 
   do {
-    cout << "\n" << question << "\n";
+    cout << "\n" << question << "\n" << "Enter 0 to QUIT" << "\n";
     cin >> inp;
+    if (inp == 0){
+      cout << "QUITTING\n\nGoodbye";
+      exit(0);
+    }
     fail = cin.fail();
-    // define variable condition based on argument to function, checked below,
-    // ie: replacing 0 with in 1,2,3,4 or between 85-95
+    // cout << "\n" << inp << "\n" << inp % 100 << "\n";
     if (typeid(inp) == typeid(int) && !func(inp)) {
       fail = true;
     }
     if (fail) {
       cin.clear();
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cout << "\n\033[1;31mInvalid input, enter a positive number\033[0m\n";
+      cout << "\n\033[1;31mInvalid input, please follow input requirements\033[0m\n";
     }
   } while (fail);
 
@@ -33,11 +49,17 @@ int getInp(string question, bool (*func)(int)) {
 }
 
 int main() {
-  std::cout << "Hello World!\n";
   // Map for associating input with correct make and further details, ie jag can
   // only have 6 cylinders
   // https://stackoverflow.com/questions/64681330/how-to-map-multiple-values-in-c
-  int makeNum = getInp("What is the make of the car? Please enter 1, 2, 3 or 4 for \n\n1)Ford\n2)Nisan\n3)Volvo\n4)Jaguar", makeValidate);
-  string carMake = "f";
-  // int yearMade = getInp("What was the year of manufacture? Note that only models made between 1995 and 2015 are supported by this service");
+  
+  int makeNum = getInp("What is the make of the car? Please enter 1, 2, 3 or 4 for \n\n1) Ford\n2) Nisan\n3) Volvo\n4) Jaguar", makeValidate);
+  string carMake = getMake(makeNum);
+  char carChar = carMake[0];
+  int yearMade = getInp("What was the year of manufacture? Note that only models made between 1995 and 2015 are supported by this service\n\nPlease enter an integer between 1995 and 2015", yearValidate);
+  string strYear = to_string(yearMade).substr(2,3);
+
+  string modelNum = carChar + strYear;
+  cout << "Your car filter model number is: "<<modelNum;
+  cout << "\nThank you for using our service\n\nGoodbye";
 }
